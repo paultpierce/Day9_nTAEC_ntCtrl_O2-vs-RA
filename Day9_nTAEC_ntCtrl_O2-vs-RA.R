67e0badcd644995546c4cd7f04b39b033ab49a47
Paul T. Pierce <paulthomaspierce@gmail.com>
2026-01-28 22:49:29 -0600

    Loading in all the libraries needed for analysis

--- Day9_nTAEC_ntCtrl_O2-vs-RA.R
+++ Day9_nTAEC_ntCtrl_O2-vs-RA.R
@@ -1,10 +1,27 @@
-# Install / load packages needed
+### Load packages needed
+
+# Differential expression analysis
+library(DESeq2)
+
+# Data handling & plotting
+library(tidyverse)
+
+# Log fold change shrinkage
+library(apeglm)
+
+# Annotation database human
+library(org.Hs.eg.db)
+
+# Visualizations
+library(RColorBrewer)
+library(pheatmap)
+library(EnhancedVolcano)
 
 
 # Import count data (raw counts file)
 
 
-# Import sample metadata (sample info)
+# Create sample metadata (sample info) including tech replicates
 
 
 # 

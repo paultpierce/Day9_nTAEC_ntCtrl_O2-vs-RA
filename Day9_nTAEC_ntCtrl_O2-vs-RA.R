320ff088f21c6bcd419d5a6c2ad4634f13ea6da4
Paul T. Pierce <paulthomaspierce@gmail.com>
2026-01-30 12:52:09 -0600

    Made dds object

--- Day9_nTAEC_ntCtrl_O2-vs-RA.R
+++ Day9_nTAEC_ntCtrl_O2-vs-RA.R
@@ -31,8 +31,37 @@ head(metadata)
 rownames(metadata) = metadata$sample_ID
 head(metadata)
 
+# Check to see if rownames from metadata and colnames of counts match for creating DESeq object 
+
+all(rownames(metadata) == colnames(counts))
 
 # Turn condition into a factor with levels for metadata
+
+metadata$condition = factor(metadata$condition, levels = c("O2", "RA"))
+metadata$condition
+
+
+# Spot checking expression for oxidative stress gene (HMOX1)
+
+gene_id = genes$GeneIds[genes$Gene == "SFTPC"]
+gene_counts = counts[gene_id, ]
+
+gene_data = cbind(metadata, counts = as.numeric(gene_counts))
+
+
+ggplot(gene_data, aes(x = condition, y = counts, fill = condition)) +
+        geom_boxplot() +
+        theme_bw(base_size = 14) +
+        xlab("SFTPC") +
+        labs(title = paste0(gene_id, " raw counts by condition"))
+
+
+# Creating dds object
+
+dds <- DESeqDataSetFromMatrix(countData = counts,
+                              colData = metadata,
+                              design = ~ condition)
+
 
 
 
